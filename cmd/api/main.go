7ac17d15a0a2d332d3f0e565ee4b9fa1b72aeaf4
Baha Bouali <mohamedbahaeddine.bouali@insat.ucar.tn>
2024-02-29 14:11:16 -0600

    added sending background emails for new users

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,10 +5,12 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	"flickvault.baha.tn/internal/data"
 	"flickvault.baha.tn/internal/jsonlog"
+	"flickvault.baha.tn/internal/mailer"
 	_ "github.com/lib/pq"
 )
 
@@ -28,12 +30,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -52,6 +63,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "2106905dffcaeb", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "9e53b32a12772b", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Flickvault <no-reply@flickvault.baha.tn>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -69,6 +86,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module flickvault.baha.tn
 go 1.21.1
 
 require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
