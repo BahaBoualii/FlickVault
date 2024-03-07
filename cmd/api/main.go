db4c3f70e6892bde5c8aa5169a373d3e7c420052
Baha Bouali <mohamedbahaeddine.bouali@insat.ucar.tn>
2024-03-06 18:04:11 -0600

    managed CORD requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -68,6 +72,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "2106905dffcaeb", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "9e53b32a12772b", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Flickvault <no-reply@flickvault.baha.tn>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(s string) error {
+		cfg.cors.trustedOrigins = strings.Fields(s)
+		return nil
+	})
 
 	flag.Parse()
 
